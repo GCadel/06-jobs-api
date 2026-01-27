2a50045b194c156a2d586eb217311f6c96bcfbd1
GCMoony <47188474+GCMoony@users.noreply.github.com>
2026-01-27 14:43:09 -0700

    Setup user creation with password hash middleware

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const email_validation_regex =
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name required"],
+    lowercase: true,
+    minLength: [3, "Name must be at least 3 characters long"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email required"],
+    minLength: [3, "Email must be at least 3 characters long"],
+    match: [email_validation_regex, "Invalid email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password required"],
+    minLength: [6, "Name must be at least 6 characters long"],
+  },
+});
+
+// 'function' keyword ties the function to the User object, allows use of 'this'
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+module.exports = model("User", UserSchema);
